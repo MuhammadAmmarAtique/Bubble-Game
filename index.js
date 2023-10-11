c7fdc6ea4b512f41e680ad00c08567916f2e99f4
MuhammadAmmarAtique <131598704+MuhammadAmmarAtique@users.noreply.github.com>
2023-10-11 11:35:50 +0500

    Created bubbles inside page agian using js (way#2)

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
 // Creating bubbles inside bottom panel using JS loop (way#1)
 
-for (let i = 1; i < 71; i++) 
-{
-   let panelbottom=document.querySelector('.panelbottom');
+// for (let i = 1; i < 71; i++) 
+// {
+//    let panelbottom=document.querySelector('.panelbottom');
 
-   //Creating a new element (bubble-element)
-   let newbubble= document.createElement('div');
-   //Adding a "bubble class in it."
-   newbubble.classList.add("bubble");
-   //Adding "numbering" it.
-   newbubble.innerHTML= i ;
-   //Making this new created element as child of "panel-bottom"  
-   panelbottom.append(newbubble);
+//    //Creating a new element (bubble-element)
+//    let newbubble= document.createElement('div');
+//    //Adding a "bubble class in it."
+//    newbubble.classList.add("bubble");
+//    //Adding "numbering" it.
+//    newbubble.innerHTML= i ;
+//    //Making this new created element as child of "panel-bottom"  
+//    panelbottom.append(newbubble);
     
+// }
+
+// Creating bubbles inside bottom panel using JS loop (way#2)
+
+let container="";
+
+for (let i = 1; i <= 144; i++) 
+{
+    // use += so that previous value doesnot got overwritten by new one.
+    container += `<div class="bubble">${i}</div>`;
+    document.querySelector('.panelbottom').innerHTML=container;
 }
